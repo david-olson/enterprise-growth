1ebfa32d7911bf58b80b67165e480951dfc6b069
David Olson <david_g_olson@premierinc.com>
2016-11-15 17:25:58 -0500

    Add radio button and button functionality

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,28 @@
 window.onload = main();
 
 function main() {
+    var radios = document.getElementsByClassName('answer_button');
+    for (var i = 0; i < radios.length; i++) {
+        radios[i].addEventListener('click', function() {
+            radioCheck(this);
+        })
+    }
+}
 
+function radioCheck(elem) {
+    var theId,
+        theQuestion,
+        theAnswer,
+        theRadio,
+        theAnswerButtons,
+        theButton;
+    theId = elem.id;
+    theQuestion = theId.charAt(1);
+    theAnswer = theId.substr(-1, 1);
+    theAnswerButtons = 'question' + theQuestion + '_buttons';
+    theRadio = 'q' + theQuestion + 'a' + theAnswer;
+    theRadio = document.getElementById(theRadio);
+    $('.' + theAnswerButtons).removeClass('answerChecked');
+    $(elem).addClass('answerChecked');
+    theRadio.checked = true;
 }
