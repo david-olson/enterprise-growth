1f8e56b26bd892b0c44a3ca1da4d914a58d99cad
David Olson <david_g_olson@premierinc.com>
2016-12-08 17:37:02 -0500

    Add scroll position events

--- assets/js/custom/main.js
+++ assets/js/custom/main.js
@@ -9,24 +9,36 @@ var panel1Height,
     panel2Active = false,
     panel3Active = false,
     panel4Active = false,
-    panel5Active = false;
+    panel5Active = false,
+    activePanels = [false, false, false, false, false];
 
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    
-    console.log(scroll);
-    console.log(panel1Height + ' ' + panel2Height);
-    
-    if (parseInt(scroll) < parseInt(panel1Height)) {
-        if (panel1Active = false) {
-            alert("Panel 1 in view!");
-            panel1Active = true;
-        }
-    } 
+$(window).scroll(function () {
+    setActivePanel();
+    //    var scroll = $(window).scrollTop();
+
+
+    //    console.log(scroll);
+    //    console.log(panel1Height + ' ' + panel2Height);
+
+    //    if (parseInt(scroll) > parseInt((panel1Height - 1)) && parseInt(scroll) < parseInt((panel2Height - 1)) && panel1Active === false) {
+    //        console.log('Panel 1 Active');
+    //        panel1Active = true;
+    //        panel2Active = false;
+    //        panel3Active = false;
+    //        panel4Active = false;
+    //        panel5Active = false;
+    //    } else if (parseInt(scroll) > parseInt(panel2Height - 1) && parseInt(scroll) < parseInt(panel3Height - 1) && panel2Active === false) {
+    //        console.log('Panel 2 Active');
+    //        panel1Active = false;
+    //        panel2Active = true;
+    //        panel3Active = false;
+    //        panel4Active = false; 
+    //        panel5Active = false;
+    //    }
 });
 
 function main() {
-    
+
     var radios = document.getElementsByClassName('answer_button'),
         submitButtons = document.getElementsByClassName('submit_answer'),
         backButtons = document.getElementsByClassName('back_button'),
@@ -94,11 +106,11 @@ function answerSubmit(elem) {
     }, 150);
     //    updatePercentage(thisQuestion);
     newUpdatePercentage(thisQuestion);
-    brickDrop(thisQuestion);
+    //    brickDrop(thisQuestion);
     min = Math.ceil(1);
     max = Math.floor(4);
-    soundNumber = Math.floor(Math.random() * (max-min + 1)) + min;
-    var theSound = new Audio('./assets/audio/'+soundNumber + '.mp3');
+    soundNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+    var theSound = new Audio('./assets/audio/' + soundNumber + '.mp3');
     theSound.play();
 }
 
@@ -120,7 +132,7 @@ function answerBack(elem) {
         scrollTop: lastQuestionOffsetTop + 'px'
     });
     newUpdatePercentage((lastQuestion - 1));
-    brickUp(thisQuestion - 1);
+    //    brickUp(thisQuestion - 1);
 }
 
 function setQuestionHeights() {
@@ -137,15 +149,15 @@ function setQuestionHeights() {
             theHeight = questionHolders[x].clientHeight;
         }
     }
-    
+
     panel1Height = document.getElementById('question-1').offsetTop;
     panel2Height = document.getElementById('question-2').offsetTop;
     panel3Height = document.getElementById('question-3').offsetTop;
     panel4Height = document.getElementById('question-4').offsetTop;
     panel5Height = document.getElementById('question-5').offsetTop;
-    
+
     setTimeout(function () {
-        
+
         for (y = 0; y < questionHolders.length; y++) {
             questionHolders[y].style.height = theHeight + 'px';
         }
@@ -198,7 +210,7 @@ function brickDrop(questionNumber) {
         break;
     case 4:
         color = 'yellow';
-        nextColor = 'blue'
+        nextColor = 'blue';
         break;
     case 5:
         color = 'blue';
@@ -218,7 +230,7 @@ function brickDrop(questionNumber) {
         if (nextBrick !== null) {
             TweenLite.to(nextBrick, .25, {
                 y: '-105%'
-            })
+            });
         }
     }, 500);
 }
@@ -230,44 +242,101 @@ function brickUp(questionNumber) {
         lastBrick;
     questionNumber = parseInt(questionNumber);
     switch (questionNumber) {
-        case 0 :
-            color = 'red';
-            lastColor = false;
-            break;
-        case 1 :
-            color = 'orange';
-            lastColor = 'red';
-            break;
-        case 2 :
-            color = 'green';
-            lastColor = 'orange'
-            break;
-        case 3 :
-            color = 'yellow';
-            lastColor = 'green';
-            break;
-        case 4 :
-            color = 'blue';
-            lastColor = 'yellow';
-            break;
-        case 5 :
-            color = false;
-            lastColor = 'blue';
-            break;
+    case 0:
+        color = 'red';
+        lastColor = false;
+        break;
+    case 1:
+        color = 'orange';
+        lastColor = 'red';
+        break;
+    case 2:
+        color = 'green';
+        lastColor = 'orange'
+        break;
+    case 3:
+        color = 'yellow';
+        lastColor = 'green';
+        break;
+    case 4:
+        color = 'blue';
+        lastColor = 'yellow';
+        break;
+    case 5:
+        color = false;
+        lastColor = 'blue';
+        break;
     }
     brick = document.getElementById(color);
     lastBrick = document.getElementById(lastColor);
-    
+
     if (brick !== null) {
         TweenLite.to(brick, .25, {
             y: '-900%'
         });
+        console.log('Fly Away');
     }
-    setTimeout(function() {
+    setTimeout(function () {
         if (lastBrick !== null) {
             TweenLite.to(lastBrick, .25, {
                 y: '-105%'
             });
+            console.log('Jump');
         }
     }, 500);
+}
+
+function setActivePanel() {
+    var scroll = $(window).scrollTop();
+
+
+
+    if (parseInt(scroll) > parseInt((panel1Height - 1)) && parseInt(scroll) < parseInt((panel2Height - 1)) && activePanels[0] === false) {
+        console.log("Panel 1 is Active");
+        togglePanelStates(0);
+    } else if (parseInt(scroll) > parseInt((panel2Height - 1)) && parseInt(scroll) < parseInt((panel3Height - 1)) && activePanels[1] === false) {
+        console.log("Panel 2 is Active");
+        togglePanelStates(1);
+    } else if (scroll > panel3Height - 1 && scroll < panel4Height - 1 && activePanels[2] === false) {
+        console.log('Panel 3 is Active');
+        togglePanelStates(2);
+    } else if (scroll > panel4Height - 1 && scroll < panel5Height - 1 && activePanels[3] === false) {
+        console.log('Panel 4 is Active');
+        togglePanelStates(3);
+    }
+
+    function togglePanelStates(activePanel) {
+        var lastPanel = 0;
+        
+        //I honestly have no idea why this last panel bullshit doesnt work. Next step is to refactor the brickDrop/Up Functions into one function and see if I can figure some things out. 
+
+        for (x = 0; x < 5; x++) {
+
+            if (x === activePanel) {
+                activePanels[x] = true;
+
+                if (x >= lastPanel) {
+                    brickDrop(x);
+                    console.log(lastPanel);
+                } else {
+                    brickUp(x);
+                    console.log(lastPanel);
+                }
+
+            } else {
+                activePanels[x] = false;
+            }
+        }
+    }
+
+    function changeBrick(qNumber, prevPanel) {
+        var color,
+            nextColor,
+            lastColor,
+            brick,
+            nextBrick,
+            lastBrick;
+        
+    }
+
 }
