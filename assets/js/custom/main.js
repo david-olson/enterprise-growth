c663b459614eea10b72f8a2d17fa31f1e49dc986
David Olson <david_g_olson@premierinc.com>
2016-12-13 17:17:56 -0500

    Add in questions, create selection validation

--- assets/js/custom/main.js
+++ assets/js/custom/main.js
@@ -11,18 +11,45 @@ var panel1Height,
     panel4Active = false,
     panel5Active = false,
     activePanels = [false, false, false, false, false],
-    activeHistory = [-1];
+    activeHistory = [-1],
+    running = false;
 
-$(window).scroll(function () {
-    setActivePanel();
+var timer;
+
+$(window).bind('scroll', function () {
+    clearTimeout(timer);
+    timer = setTimeout(setActivePanel, 250);
 });
+
+var refresh = function () {
+    console.log('stopped');
+}
+
+//$(window).scroll(function () {
+//    if (running === false) {
+//        setActivePanel();
+//    }
+//});
 
 function main() {
 
     var radios = document.getElementsByClassName('answer_button'),
         submitButtons = document.getElementsByClassName('submit_answer'),
         backButtons = document.getElementsByClassName('back_button'),
+        introButton = document.getElementById('intro-button'),
         windowH = window.innerHeight;
+    introButton.addEventListener('click', function() {
+       var firstQuestion = document.getElementById('question-1');
+        
+        firstQuestion.style.display = 'block';
+        setTimeout(function() {
+            var firstQuestionOffset = firstQuestion.offsetTop;
+            $('html, body').animate({
+                scrollTop: firstQuestionOffset + 'px'
+            });
+        }, 250);
+        
+    });
     for (i = 0; i < radios.length; i++) {
         radios[i].addEventListener('click', function () {
             radioCheck(this);
@@ -70,28 +97,38 @@ function answerSubmit(elem) {
         nextQuestionObject,
         nextQuestionOffsetTop,
         scrollSpeed,
-        soundNumber;
+        soundNumber,
+        theRadios,
+        i;
     aId = elem.id;
     thisQuestion = aId.substr(-1, 1);
     nextQuestion = parseInt(thisQuestion) + 1;
     nextQuestionId = 'question-' + nextQuestion;
     nextQuestionObject = document.getElementById(nextQuestionId);
+    theRadios = document.getElementsByName('question' + thisQuestion);
 
-    nextQuestionObject.style.display = 'block';
-    setTimeout(function () {
-        nextQuestionOffsetTop = nextQuestionObject.offsetTop;
-        $('html, body').animate({
-            scrollTop: nextQuestionOffsetTop + 'px'
-        });
-    }, 150);
-    //    updatePercentage(thisQuestion);
-//    newUpdatePercentage(thisQuestion);
-    //    brickDrop(thisQuestion);
-    min = Math.ceil(1);
-    max = Math.floor(4);
-    soundNumber = Math.floor(Math.random() * (max - min + 1)) + min;
-    var theSound = new Audio('./assets/audio/' + soundNumber + '.mp3');
-    theSound.play();
+    for (i = 0; i < theRadios.length; i++){
+        if(theRadios[i].checked){
+            nextQuestionObject.style.display = 'block';
+            setTimeout(function () {
+                nextQuestionOffsetTop = nextQuestionObject.offsetTop;
+                $('html, body').animate({
+                    scrollTop: nextQuestionOffsetTop + 'px'
+                });
+            }, 150);
+            //    updatePercentage(thisQuestion);
+            //    newUpdatePercentage(thisQuestion);
+            //    brickDrop(thisQuestion);
+            min = Math.ceil(1);
+            max = Math.floor(4);
+            soundNumber = Math.floor(Math.random() * (max - min + 1)) + min;
+            var theSound = new Audio('./assets/audio/' + soundNumber + '.mp3');
+            theSound.play();
+            break;
+        } else if(i === (theRadios.length - 1)) {
+            alert('Not so fast');
+        }
+    }
 }
 
 function answerBack(elem) {
@@ -111,7 +148,7 @@ function answerBack(elem) {
     $('html, body').animate({
         scrollTop: lastQuestionOffsetTop + 'px'
     });
-//    newUpdatePercentage((lastQuestion - 1));
+    //    newUpdatePercentage((lastQuestion - 1));
     //    brickUp(thisQuestion - 1);
 }
 
@@ -170,6 +207,7 @@ function brickDrop(questionNumber) {
         nextColor,
         brick,
         nextBrick;
+    running = true;
     questionNumber = parseInt(questionNumber);
     switch (questionNumber) {
     case 0:
@@ -212,6 +250,7 @@ function brickDrop(questionNumber) {
                 y: '-105%'
             });
         }
+        running = false;
     }, 500);
 }
 
@@ -220,6 +259,7 @@ function brickUp(questionNumber) {
         lastColor,
         brick,
         lastBrick;
+    running = true;
     questionNumber = parseInt(questionNumber);
     switch (questionNumber) {
     case 0:
@@ -261,6 +301,7 @@ function brickUp(questionNumber) {
                 y: '-105%'
             });
         }
+        running = false;
     }, 500);
 }
 
@@ -273,26 +314,31 @@ function setActivePanel() {
         y = activeHistory.slice(-1)[0];
         togglePanelStates(0, y);
         newUpdatePercentage(0);
+        positionBricks(1);
     } else if (scroll > panel2Height - 100 && scroll < panel3Height - 100 && activePanels[1] === false) {
         console.log("Panel 2 is Active");
         y = activeHistory.slice(-1)[0];
         togglePanelStates(1, y);
         newUpdatePercentage(1);
+        positionBricks(2);
     } else if (scroll > panel3Height - 100 && scroll < panel4Height - 100 && activePanels[2] === false) {
         console.log('Panel 3 is Active');
         y = activeHistory.slice(-1)[0];
         togglePanelStates(2, y);
         newUpdatePercentage(2);
+        positionBricks(3);
     } else if (scroll > panel4Height - 100 && scroll < panel5Height - 100 && activePanels[3] === false) {
         console.log('Panel 4 is Active');
         y = activeHistory.slice(-1)[0];
         togglePanelStates(3, y);
         newUpdatePercentage(3);
+        positionBricks(4);
     } else if (scroll > panel5Height - 100 && activePanels[4] === false) {
         console.log('Panel 5 is Active');
         y = activeHistory.slice(-1)[0];
         togglePanelStates(4, y);
         newUpdatePercentage(4);
+        positionBricks(5);
     }
 
     function togglePanelStates(activePanel, lastPanel) {
@@ -301,29 +347,79 @@ function setActivePanel() {
 
             if (x === activePanel) {
                 activePanels[x] = true;
-                if (activePanel > lastPanel) {
-                    brickDrop(activePanel);
-                    setTimeout(function () {
-                        activeHistory.push(activePanel);
-                    }, 100);
-                } else {
-                    brickUp(activePanel + 1);
-                    activeHistory.push(activePanel);
-                }
+//                if (activePanel > lastPanel) {
+//                    //                    brickDrop(activePanel);
+//                    setTimeout(function () {
+//                        activeHistory.push(activePanel);
+//                    }, 100);
+//                } else {
+//                    //                    brickUp(activePanel + 1);
+//                    activeHistory.push(activePanel);
+//                }
             } else {
                 activePanels[x] = false;
             }
         }
     }
 
-    function changeBrick(qNumber, prevPanel) {
-        var color,
-            nextColor,
-            lastColor,
-            brick,
-            nextBrick,
-            lastBrick;
+    function positionBricks(panelActive) {
+        var red = document.getElementById('red'),
+            orange = document.getElementById('orange'),
+            green = document.getElementById('green'),
+            yellow = document.getElementById('yellow'),
+            blue = document.getElementById('blue');
 
+        switch (panelActive) {
+        case 1:
+            TweenLite.to(red, .25, {
+                y: '-105%'
+            });
+            TweenLite.to([orange, green, yellow, blue], .25, {
+                y: '-900%'
+            });
+            break;
+        case 2:
+            TweenLite.to(red, .25, {
+                y: '0%'
+            });
+            TweenLite.to(orange, .25, {
+                y: '-105%'
+            });
+            TweenLite.to([green, yellow, blue], .25, {
+                y: '-900%'
+            });
+            break;
+        case 3:
+            TweenLite.to([red, orange], .25, {
+                y: '0%'
+            });
+            TweenLite.to(green, .25, {
+                y: '-105%'
+            });
+            TweenLite.to([yellow, blue], .25, {
+                y: '-900%'
+            });
+            break;
+        case 4:
+            TweenLite.to([red, orange, green], .25, {
+                y: '0%'
+            });
+            TweenLite.to(yellow, .25, {
+                y: '-105%'
+            });
+            TweenLite.to(blue, .25, {
+                y: '-900%'
+            });
+            break;
+        case 5:
+            TweenLite.to([red, orange, green, yellow], .25, {
+                y: '0%'
+            });
+            TweenLite.to(blue, .25, {
+                y: '-105%'
+            });
+            break;
+        }
     }
 
 }
